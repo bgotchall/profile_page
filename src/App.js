0b514267aff90874023a31b9829faa1e37434583
bgotchall <bgotchall@gmail.com>
2020-03-31 12:10:18 -0500

    removed unneeded pages from app.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
- import Blog from "./components/pages/Blog";
+ //import Blog from "./components/pages/Blog";
  import Contact from "./components/pages/Contact";
  import Portfolio from "./components/pages/Portfolio";
- import Skills from "./components/pages/Skills";
+ //import Skills from "./components/pages/Skills";
  //import NavTabs from './components/NavTabs';
 
 function App() {
@@ -18,9 +18,9 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="http://www.bgotchall.com" component={Home} />
           <Route exact path="/about" component={About} />
-          <Route exact path="/blog" component={Blog} />
+         
           <Route exact path="/portfolio" component={Portfolio} />
-          <Route exact path="/skills" component={Skills} />
+          
           <Route path="/contact" component={Contact} />
         </div>
       </Router>
