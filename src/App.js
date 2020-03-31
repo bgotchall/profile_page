41e78d67b6164de0c5a9b4fb43b20c38b4290bda
bgotchall <bgotchall@gmail.com>
2020-03-30 20:25:07 -0500

    testing rollback

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-// import Blog from "./components/pages/Blog";
+//import About from "./components/pages/About";
+ import Blog from "./components/pages/Blog";
 // import Contact from "./components/pages/Contact";
 // import Portfolio from "./components/pages/Portfolio";
 // import Skills from "./components/pages/Skills";
@@ -15,9 +15,9 @@ function App() {
       <Router>
         <div>
         
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Blog} />
           {/* <Route exact path="http://www.bgotchall.com" component={Home} /> */}
-          <Route exact path="/about" component={About} />
+          {/* <Route exact path="/about" component={About} /> */}
           {/* <Route exact path="/blog" component={Blog} />
           <Route exact path="/portfolio" component={Portfolio} />
           <Route exact path="/skills" component={Skills} />
