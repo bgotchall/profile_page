5052ccfc2576d563edf3c61a579baf15e6cfefe0
bgotchall <bgotchall@gmail.com>
2020-03-30 20:05:14 -0500

    ran build

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// var express = require('express');
+// var app = express();
+// var http = require('http').Server(app);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +20,9 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+// http.listen(process.env.PORT || 3000, function(){
+//   console.log('listening on', http.address().port);
+// });
