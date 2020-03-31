119664fd6cac38b6b14a68a809736456355d54e8
bgotchall <bgotchall@gmail.com>
2020-03-31 10:31:43 -0500

    restored to working locally

--- dev_notes.md
+++ dev_notes.md
@@ -9,3 +9,17 @@ to do:  figure out why the portfolio tiles are not wrapping.
 add skills/experience section
 
 add blog section to flesh do little articles
+
+
+late 3/30 quitting:
+
+nothing will deploy.  fernando on slack sent me how to's that don't work.  some simple react apps just work, and others don't.  they cannot ALL need these instructions.  The instructions also don't work.  cannot decode the fucking %public_URL% thing.  
+
+tried to look at the MERN example but it didn't help.
+
+tasks 
+1) clean up the repo.  I fucked it up trying to get it to work.  do a branch next time to mess with it.
+2) try to deploy the mern example app.  
+3) go back to the experiment where I start with a basic react app ans slowly add content.  the odd thing is that the default app did not seem to work, yet the "routing study" did...
+4) modify routing study to have my content?  first pass this shows promise!
+
--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-//import Home from "./components/pages/Home";
-//import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
  import Blog from "./components/pages/Blog";
-// import Contact from "./components/pages/Contact";
-// import Portfolio from "./components/pages/Portfolio";
-// import Skills from "./components/pages/Skills";
-// import NavTabs from './components/NavTabs';
+ import Contact from "./components/pages/Contact";
+ import Portfolio from "./components/pages/Portfolio";
+ import Skills from "./components/pages/Skills";
+ //import NavTabs from './components/NavTabs';
 
 function App() {
   return (
     <>
-    <p> I am an app</p>
       <Router>
         <div>
         
-          <Route exact path="/" component={Blog} />
-          {/* <Route exact path="http://www.bgotchall.com" component={Home} /> */}
-          {/* <Route exact path="/about" component={About} /> */}
-          {/* <Route exact path="/blog" component={Blog} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="http://www.bgotchall.com" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/blog" component={Blog} />
           <Route exact path="/portfolio" component={Portfolio} />
           <Route exact path="/skills" component={Skills} />
-          <Route path="/contact" component={Contact} /> */}
+          <Route path="/contact" component={Contact} />
         </div>
       </Router>
     </>
