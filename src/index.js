7ecbb77c793e1fd283bed2243fb17c7706f1d9c4
bgotchall <bgotchall@gmail.com>
2020-03-31 11:52:42 -0500

    changed the reactDom.render call in index.js to match the target repo

--- src/index.js
+++ src/index.js
@@ -5,24 +5,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// var express = require('express');
-// var app = express();
-// var http = require('http').Server(app);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-
-
-// http.listen(process.env.PORT || 3000, function(){
-//   console.log('listening on', http.address().port);
-// });
